67ed8dc30e049897d3531c73d07ad49bedc1a3b4
alvaro <alvarorajic2@gmail.com>
2022-10-23 22:44:44 +0200

    added modal functionality for the director

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Arnold Client
+
+- Continuation of the first part of the MERN project
+- Displays a list of movies in which Schwarzenegger has starred (not all of them, just a sample)
+- Also displays directors and genres
+- Still in progress... (login authentication still not implemented)
--- src/components/MovieView/MovieView.js
+++ src/components/MovieView/MovieView.js
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./MovieView.css";
 
+// modal
+import Modal from "../../Modal";
+
 const MovieView = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState("");
+  const [modal, showModal] = useState(false);
+
+  const handleModal = () => {
+    showModal((prevState) => !prevState);
+  };
 
   const fetchMovie = async () => {
     const res = await fetch(
@@ -34,7 +42,7 @@ const MovieView = () => {
           </div>
           <div className="director">
             <h3>Director</h3>
-            <span>{movie.director.name}</span>
+            <span onClick={handleModal}>{movie.director.name}</span>
           </div>
           <div className="genre">
             <h3>Genre</h3>
@@ -46,6 +54,28 @@ const MovieView = () => {
           <h1>Description</h1>
           <span>{movie.description}</span>
         </div>
+
+        {modal ? (
+          <Modal>
+            <div
+              style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+            >
+              <h1>{movie.director.name}</h1>
+
+              <h2>Birth - {new Date(movie.director.birth).toDateString()}</h2>
+              <h2>
+                Death -{" "}
+                {movie.director.deatch
+                  ? new Date(movie.director.death).toDateString()
+                  : ""}
+              </h2>
+              <h2>Bio</h2>
+              <h3>{movie.director.bio}</h3>
+
+              <button onClick={handleModal}>Close</button>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     )
   );
